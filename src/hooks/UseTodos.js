bacc93f125bada6c2a2fcd72029022eac5803f44
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-09-29 15:36:35 -0700

    Reduce UI delay from backend trips
    Now we just modify the local state and hope that 1) we're paying
    attention and doing the state transitions right and 2) the backend never
    fails.
    
    It would be nice to add a state rollback feature in the case that the
    backend errors out on a request, so the UI can update immediately but
    roll back if an error occurs.

--- src/hooks/UseTodos.js
+++ src/hooks/UseTodos.js
@@ -7,30 +7,29 @@ export default function useTodos() {
   // after each state transition before updating the UI adds a significant
   // delay to the app. Find another way to doo this.
   const [todos, setTodos] = useState([]);
-  const [version, setVersion] = useState(0);
 
   useEffect(() => {
     TodosService.getAllTodos()
       .then(todos => setTodos(todos))
       // eslint-disable-next-line no-console
       .catch(error => console.log(error));
-  }, [version]);
+  }, []);
 
   async function addTodo(description) {
-    await TodosService.addTodo(description);
-    setVersion(version => version + 1);
+    const newTodo = await TodosService.addTodo(description);
+    setTodos(prev => [...prev, newTodo]);
   }
 
   async function toggleTodoCompleted(id) {
+    setTodos(prev => prev.map(x => x.id === id ? { ...x, complete: !x.complete } : { ...x }));
+
     const todo = todos.find(x => x.id === id);
-    todo.complete = !todo.complete;
-    await TodosService.updateTodo(todo);
-    setVersion(version => version + 1);
+    await TodosService.updateTodo({ ...todo, complete: !todo.complete });
   }
 
   async function deleteTodo(id) {
+    setTodos(prev => prev.filter(x => x.id !== id));
     await TodosService.deleteTodo(id);
-    setVersion(version => version + 1);
   }
 
   return { todos, addTodo, toggleTodoCompleted, deleteTodo };
